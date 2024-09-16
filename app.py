9e3106aa5d2ca432c6e76ee8a9b39ddf7848baf9
laistdomiciano <laistdomiciano@gmail.com>
2024-09-16 14:07:35 +0200

    readme

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 app.secret_key = os.environ.get('FRONTEND_SECRET_KEY', '1234secret')
 
-
-# BACKEND_API_URL = os.environ.get('BACKEND_API_URL', 'http://localhost:5002')
 BACKEND_API_URL = os.environ.get('BACKEND_API_URL', 'https://ecgenerator-backend.onrender.com')
 
 
